905832099cd2e31cab6e6e98086dd0bbf7c61858
Spencer, Paul Whiting <pwablito@gmail.com>
2022-10-30 22:09:29 -0400

    Initialize node module, add basic neo4j stuff from website

---
+++ main.js
@@ -0,0 +1,30 @@
+const neo4j = require('neo4j-driver')
+
+
+const uri = "neo4j://neo4j:7687";
+const user = "neo4j";
+const password = "password";
+
+async function main() {
+    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+    const session = driver.session()
+    const personName = 'Alice'
+
+    try {
+        const result = await session.run(
+            'CREATE (a:Person {name: $name}) RETURN a', { name: personName }
+        )
+
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+
+        console.log(node.properties.name)
+    } finally {
+        await session.close()
+    }
+
+    // on application exit:
+    await driver.close()
+}
+
+main()
