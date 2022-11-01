59b393400f381b0eed07ec1aeb1bcfb4404f8873
Spencer, Paul Whiting <pwablito@gmail.com>
2022-10-31 23:22:47 -0400

    Use environment variables for simple script

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
 const neo4j = require('neo4j-driver')
 
-
-const uri = "neo4j://neo4j:7687";
-const user = "neo4j";
-const password = "password";
+const uri = process.env.NEO4J_URI || "neo4j://localhost:7687";
+const user = process.env.NEO4J_USER || "neo4j";
+const password = process.env.NEO4J_PASSWORD || "password";
 
 async function main() {
+    console.log("Welcome to the neo4j-powered node for blockchain social media")
     const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
     const session = driver.session()
-    const personName = 'Alice'
-
     try {
-        const result = await session.run(
-            'CREATE (a:Person {name: $name}) RETURN a', { name: personName }
+        let result = await session.run(
+            'CREATE (a:Person {name: $name1}) -[r:RESPECTS]-> (b:Person {name: $name2}) RETURN a, b, r', {
+                name1: "Bob",
+                name2: "Alice",
+            }
         )
-
-        const singleRecord = result.records[0]
-        const node = singleRecord.get(0)
-
-        console.log(node.properties.name)
+        console.dir(result.records, { depth: null })
     } finally {
         await session.close()
     }
-
-    // on application exit:
     await driver.close()
 }
 
