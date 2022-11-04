1004f119df61e191ac5f37f00bf040692307dbcf
Paul Spencer <32964672+pwablito@users.noreply.github.com>
2022-11-04 09:25:06 -0400

    Add notes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ async function main() {
 
     // TODO Start block creation loop and expose an interface for CRUD on stored data
 
+    // Start CRUD api, on any update, send the result to other nodes
+
+    while (true) {
+        // Gather unattached objects from database
+        // Try solving a hash puzzle
+        // Send result, unless interrupted by another node solving it first
+    }
+
     await mgr.close()
 }
 
