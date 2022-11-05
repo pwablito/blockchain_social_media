c30a8e33f6dd4234d0514ab4e486b6fe626773a0
Spencer, Paul Whiting <pwablito@gmail.com>
2022-11-05 12:16:33 -0400

    Fix style and logging

--- src/main.js
+++ src/main.js
@@ -12,17 +12,16 @@ async function main() {
     const api = get_api(mgr)
     const server = api.listen(api_port, () => console.log(`Started api listener on port ${api_port}`))
 
-    // Start CRUD api, on any update, send the result to other nodes
-
     let interrupt = false
     let interrupt_func = () => {
-            console.log("Got interrupt, killing program")
-            interrupt = true
-            server.close(() => {
-                "Shutting down api listener"
-            })
-        }
-        // Set signal handlers to kill main loop
+        console.log("Got interrupt, killing program")
+        interrupt = true
+        server.close(() => {
+            console.log("Shutting down api listener")
+        })
+    }
+
+    // Set signal handlers to kill main loop
     process.on('SIGINT', interrupt_func)
     process.on('SIGQUIT', interrupt_func)
     process.on('SIGTERM', interrupt_func)
@@ -35,6 +34,7 @@ async function main() {
         let profiles = mgr.get_detached_profiles()
         let posts = mgr.get_detached_posts()
         let comments = mgr.get_detached_comments()
+
 
         // Try solving a hash puzzle to generate nonce for new block
         // Send result, unless interrupted by another node solving it first
