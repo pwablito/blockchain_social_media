3ffbc114e1d7629317e919c9b017285888ce440e
Spencer, Paul Whiting <pwablito@gmail.com>
2022-11-12 01:20:15 -0500

    Quick api improvements

--- src/api.js
+++ src/api.js
@@ -5,17 +5,34 @@ import { other_nodes } from "./main.js";
 function get_api(db_manager) {
     const app = express()
     app.use(express.json())
-    app.get('/status', (_, res) => {
-        res.send({ "success": true, "status": "ok" })
+
+    app.post('/status', (_, res) => {
+        // Quick endpoint to check that the server is still up
+        // TODO add some health checks, report back their results
+        res.send({ success: true, status: "ok" })
     })
-    app.get('/post', (_, res) => {
-        db_manager.create_post("test", "123")
-        res.send({ "success": false, "error": "not implemented" })
+
+    app.post('/post', async(req, res) => {
+        try {
+            // TODO add some kind of signature validation. Signatures should end up in database too
+            let post_id = await db_manager.create_post(req.body.body, req.body.author_id)
+            res.send({ success: true, id: post_id })
+        } catch (e) {
+            console.log(e)
+            res.send({ success: false, error: e })
+        }
     })
-    app.get('/profile', async(_, res) => {
-        let profile_id = await db_manager.create_profile("123", "test", "test")
-        res.send({ success: false, id: profile_id })
+
+    app.post('/profile', async(req, res) => {
+        try {
+            let profile_id = await db_manager.create_profile(req.body.handle, req.body.pubkey, req.body.bio)
+            res.send({ success: true, id: profile_id })
+        } catch (e) {
+            console.log(e)
+            res.send({ success: false, error: e })
+        }
     })
+
     app.post('/join', (req, res) => {
         res.send({ "success": true, "nodes": other_nodes })
         if (req.body.addr) {
